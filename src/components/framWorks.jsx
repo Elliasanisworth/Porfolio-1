eb12e6ac4108998544106d979e54efbe2487d9ee
Elliasanisworth <Elliasanisworth@gmail.com>
2025-07-27 05:44:45 +0530

    About done

---
+++ src/components/framWorks.jsx
@@ -0,0 +1,41 @@
+import { OrbitingCircles } from "./OrbitCircle";
+
+
+export function Frameworks() {
+    const skills =[
+        "auth0", //
+        "html5",//
+        "css3",//
+        "javascript",//
+        "react",//
+        "nodejs",//
+        "mongodb",//
+        "tailwindcss",//
+        "microsoft",//
+        "sql",//
+        "git", //
+        "github",//
+        "vitejs",
+        "wordpress",//
+        "Java",//
+        "c", //
+    ]
+  return (
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center ">
+      <OrbitingCircles iconSize={40}>
+       {skills.map((skill, index)=>(
+        <Icon key={index} src={`assets/logos/${skill}.svg`}/>
+       ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index)=>(
+        <Icon key={index} src={`assets/logos/${skill}.svg`}/>
+       ))}
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+const Icon =({src}) =>(
+    <img src={src} className="rounded-sm hover:scale-110 duration-200" />
+);
